57200447caf7750fc7bf73864d34dd986c519383
Hapiel <daniel@danielsiegmund.nl>
2023-12-14 16:41:32 +0100

    day 5 attempt. Only A works.

---
+++ day05/day05a.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm> 
+
+int main()
+{
+  std::ifstream input("input.txt");
+
+  std::vector<std::string> lines;
+  std::string line;
+
+  while (std::getline(input, line))
+  {
+    lines.push_back(line);
+  }
+
+  std::vector<long long> seeds;
+
+  std::istringstream seedStream(lines[0]);
+  std::string seed;
+
+  while (std::getline(seedStream, seed, ' '))
+  {
+    if (std::isdigit(seed[0]))
+    {
+      seeds.push_back(std::stoll(seed));
+    }
+  }
+
+  std::vector<std::vector<std::vector<long long>>> maps;
+
+  int mapCount = -1;
+
+  for (size_t i = 2; i < lines.size(); i++)
+  {
+    if (lines[i].find("map") != std::string::npos)
+    {
+      mapCount++;
+      std::vector<std::vector<long long>> map;
+      maps.push_back(map);
+    }
+    if (isdigit(lines[i][0]))
+    {
+      std::istringstream iss(lines[i]);
+      std::string number;
+      std::vector<long long> range;
+      while (std::getline(iss, number, ' '))
+      {
+        range.push_back(std::stoll(number));
+      }
+      maps[mapCount].push_back(range);
+    }
+  }
+
+  // parsing done
+
+  std::vector<long long> locations;
+
+  for (long long seed : seeds)
+  {
+    long long location = seed;
+    for (std::vector<std::vector<long long>> map : maps)
+    {
+      for (std::vector<long long> range : map)
+      {
+        if (location >= range[1] && location < range[1] + range[2]){
+          location = range[0] + (location - range[1]);
+          break;
+        }
+      }
+    }
+    locations.push_back(location);
+  }
+
+  auto nearestLocation = std::min_element(locations.begin(), locations.end());
+
+  std::cout << *nearestLocation << std::endl;
+
+  return 0;
+}
